01cf821b28aa2febf52e79d3510b8994a68af0a0
liminalvoid <usrnm1337@proton.me>
2025-10-08 20:50:14 +0300

    Fixed line too long warning

--- src/app/reports.py
+++ src/app/reports.py
@@ -9,7 +9,10 @@ def report_average_rating(data: Table) -> str:
     result: Table = [["", group_by, aggregation_column]]
 
     hashmap = get_average(data, group_by, aggregation_column)
-    hashmap_sorted = dict(sorted(hashmap.items(), key=lambda item: item[1])[::-1])
+    hashmap_sorted = dict(sorted(
+        hashmap.items(),
+        key=lambda item: item[1]
+    )[::-1])
     idx = 1
 
     for key, value in hashmap_sorted.items():
@@ -33,7 +36,10 @@ def report_average_price(data: Table) -> str:
     result: Table = [["", group_by, aggregation_column]]
 
     hashmap = get_average(data, group_by, aggregation_column)
-    hashmap_sorted = dict(sorted(hashmap.items(), key=lambda item: item[1])[::-1])
+    hashmap_sorted = dict(sorted(
+        hashmap.items(),
+        key=lambda item: item[1]
+    )[::-1])
     idx = 1
 
     for key, value in hashmap_sorted.items():
